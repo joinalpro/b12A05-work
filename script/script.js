bc73b3e7c1ac7599e56525e02768154690483729
Joinal Abden <joinalzx@gmail.com>
2025-08-28 14:54:38 +0600

    copy count innerText to number convert git add .

---
+++ script/script.js
@@ -0,0 +1,26 @@
+function getElementById(id) {
+  const element = document.getElementById(id);
+  return element;
+}
+
+getElementById("card-parent").addEventListener("click", function (e) {
+  const callHistory = getElementById("call-history-container");
+  const allHistory = document.createElement("div");
+  // convert date
+  const date = new Date().toLocaleTimeString();
+  // dynamic title
+  const title =
+    e.target.parentNode.parentNode.parentNode?.children[1]?.children[1]
+      ?.innerText;
+  // dynamic phone number
+  const phoneNumber =
+    e.target.parentNode.parentNode.parentNode?.children[1]?.children[2]
+      ?.innerText;
+
+  // copy count innerText to number convert
+  if (e.target.className.includes("copy-btn")) {
+    let copyCount = Number(getElementById("copy-count").innerText);
+    getElementById("copy-count").innerText = copyCount += 1;
+    return alert(`copy successfully! ${phoneNumber}`);
+  }
+});
